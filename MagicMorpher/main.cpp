923971fa0b4aa8e9db6042133bcd1a57099a8aa5
Zac Chan <sunshinecltzac@gmail.com>
2017-01-16 00:12:57 +0800

    Initial Commit

---
+++ MagicMorpher/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MagicMorpher
+//
+//  Created by 陈乐天 on 2017/1/16.
+//  Copyright © 2017年 陈乐天. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
